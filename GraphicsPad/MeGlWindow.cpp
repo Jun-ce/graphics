e33bd3ccbadbaa9cc5b43686bf709303fb85e84d
Junce Wang <18187396+Jun-ce@users.noreply.github.com>
2022-09-19 19:12:13 -0600

    Update to Win 10 SDK, add comments

--- GraphicsPad/MeGlWindow.cpp
+++ GraphicsPad/MeGlWindow.cpp
@@ -5,23 +5,24 @@ void MeGlWindow::initializeGL()
 {
 	glewInit();
 
-	GLfloat verts[] =
+	GLfloat verts[] =  // Normalized coordinates in window space, origion is (0,0), bottom left is (-1,-1), top right is (1,1)
 	{
 		+0.0f, +1.0f,
 		-1.0f, -1.0f,
 		+1.0f, -1.0f,
 	};
 	GLuint myBufferID;
-	glGenBuffers(1, &myBufferID);
-	glBindBuffer(GL_ARRAY_BUFFER, myBufferID);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verts),
-		verts, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glGenBuffers(1, &myBufferID);  // Ask for a buffer
+	glBindBuffer(GL_ARRAY_BUFFER, myBufferID);  // Bind it to a array buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verts),  // Set the data format of the buffer and send actual data
+		verts, 
+		GL_STATIC_DRAW);  // Overwrite data?
+	glEnableVertexAttribArray(0);  // ?
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);  // ?  Construction related?
 }
 
 void MeGlWindow::paintGL()
 {
-	glViewport(0, 0, width(), height());
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glViewport(0, 0, width(), height());  // Windows coordinate
+	glDrawArrays(GL_TRIANGLES, 0, 3);  // Draw triangles, starting with 0 idx, using 3 vectors
 }
