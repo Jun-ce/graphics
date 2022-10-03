d32c1600c56656e0008da13f242e13f9e5b2a320
Junce Wang <18187396+Jun-ce@users.noreply.github.com>
2022-10-03 16:48:08 -0600

    Transform2D definition

---
+++ GraphicsPad/Transform2D.cpp
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "Transform2D.h"
+
+Transform2D::Transform2D() :
+	position(0.0f, 0.0f),
+	rotation(0.0f),
+	scale(1.0f, 1.0f)
+{
+}
+
+void Transform2D::outGlOffset(float& x, float& y) {
+	x = this->position.x;
+	y = this->position.y;
+}
+
+void Transform2D::outSingleScale(float& s) {
+	s = this->scale.x;
+}
+
+void Transform2D::outScale(float& x, float& y) {
+	x = this->scale.x;
+	y = this->scale.y;
+}
---
+++ GraphicsPad/Transform2D.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <glm/glm.hpp>
+
+class Transform2D {
+public:
+	Transform2D();
+	glm::vec2 position;
+	float rotation;
+	glm::vec2 scale;
+	void outGlOffset(float& x, float& y);
+	void outScale(float& x, float& y);
+	void outSingleScale(float& s);
+};
