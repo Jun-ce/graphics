f1b494883d655abe6a1678c6cbb5bef84f1934e1
Junce Wang <18187396+Jun-ce@users.noreply.github.com>
2022-10-03 20:19:26 -0600

    Assignment 3
    1. Sub Data Buffer for array and element array (idx)
    2. Arrow Key control
    3. Global verts[] and indices[]

--- GraphicsPad/MeGlWindow.cpp
+++ GraphicsPad/MeGlWindow.cpp
@@ -20,10 +20,202 @@ extern float getERSymbolY(int y);
 
 #pragma endregion
 
+// Primitive Data
+#pragma region MyRegion
+
+int triNo = 30;
+
+GLfloat verts[] =
+{
+	// Data
+	// float x, float, y,
+	// float r, float g, floar b
+
+	// BG color
+	// 1.0f, 198.0f/255.0f, 14.0f/255.0f,
+
+	// Symbol color
+	// 127.0f/255.0f, 211.0f / 255.0f, 42.0f / 255.0f
+
+	// BG
+	// 0 TR 
+	+1.0f, +1.0f,
+	1.0f, 198.0f / 255.0f, 14.0f / 255.0f,
+	// 1 TL
+	-1.0f, +1.0f,
+	1.0f, 198.0f / 255.0f, 14.0f / 255.0f,
+	// 2 BL
+	-1.0f, -1.0f,
+	1.0f, 198.0f / 255.0f, 14.0f / 255.0f,
+	// 3 BR
+	+1.0f, -1.0f,
+	1.0f, 198.0f / 255.0f, 14.0f / 255.0f,
+
+	// Symbol
+	// 4
+	getERSymbolX(269),
+	getERSymbolY(326),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 5 
+	getERSymbolX(313),
+	getERSymbolY(326),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 6
+	getERSymbolX(295),
+	getERSymbolY(4),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 7
+	getERSymbolX(289),
+	getERSymbolY(639),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// R 
+	// 8
+	getERSymbolX(301),
+	getERSymbolY(320),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 9
+	getERSymbolX(300),
+	getERSymbolY(383),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 10
+	getERSymbolX(450),
+	getERSymbolY(461),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 11
+	getERSymbolX(432),
+	getERSymbolY(470),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 12
+	getERSymbolX(410),
+	getERSymbolY(427),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 13
+	getERSymbolX(331),
+	getERSymbolY(511),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 14
+	getERSymbolX(352),
+	getERSymbolY(535),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 15
+	getERSymbolX(347),
+	getERSymbolY(495),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 16
+	getERSymbolX(471),
+	getERSymbolY(674),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// E
+	// 17
+	getERSymbolX(286),
+	getERSymbolY(309),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 18
+	getERSymbolX(150),
+	getERSymbolY(251),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 19
+	getERSymbolX(284),
+	getERSymbolY(364),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 20
+	getERSymbolX(171),
+	getERSymbolY(237),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 21
+	getERSymbolX(290),
+	getERSymbolY(185),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 22
+	getERSymbolX(290),
+	getERSymbolY(165),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 23
+	getERSymbolX(175),
+	getERSymbolY(272),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 24
+	getERSymbolX(256),
+	getERSymbolY(195),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 25
+	getERSymbolX(147),
+	getERSymbolY(97),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 26
+	getERSymbolX(159),
+	getERSymbolY(92),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 27
+	getERSymbolX(295),
+	getERSymbolY(96),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 28
+	getERSymbolX(294),
+	getERSymbolY(51),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+
+	// 29
+	getERSymbolX(174),
+	getERSymbolY(115),
+	127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
+};
+
+GLushort indices[] = {
+	// BG
+	0,1,2,
+	0,2,3,
+	// Symbol
+	4,5,6,
+	4,5,7,
+	// R
+	8,9,10,
+	8, 9, 11,
+	10,12,13,
+	13,10,14,
+	13, 16, 15,
+	// E
+	17, 18, 19,
+	17, 18, 20,
+	18,21,22,
+	18,21,23,
+	21,24,25,
+	21, 25, 26,
+	25,27,28,
+	25,27,29,
+};
+
+#pragma endregion
 
 
 // GL mid
 #pragma region GlMid
+
+
 
 GLuint programID;
 GLint offsetAttribLoc;
@@ -74,174 +266,19 @@ bool checkProgramStatus(GLuint programID)
 	return checkStatus(programID, glGetProgramiv, glGetProgramInfoLog, GL_LINK_STATUS);
 }
 
+
 void sendDataToOpenGL()
 {
-	GLfloat verts[] =
-	{
-		// Data
-		// float x, float, y,
-		// float r, float g, floar b
-
-		// BG color
-		// 1.0f, 198.0f/255.0f, 14.0f/255.0f,
-
-		// Symbol color
-		// 127.0f/255.0f, 211.0f / 255.0f, 42.0f / 255.0f
-
-		// BG
-		// 0 TR 
-		+1.0f, +1.0f,
-		1.0f, 198.0f / 255.0f, 14.0f / 255.0f,
-		// 1 TL
-		-1.0f, +1.0f,
-		1.0f, 198.0f / 255.0f, 14.0f / 255.0f,
-		// 2 BL
-		-1.0f, -1.0f,
-		1.0f, 198.0f / 255.0f, 14.0f / 255.0f,
-		// 3 BR
-		+1.0f, -1.0f,
-		1.0f, 198.0f / 255.0f, 14.0f / 255.0f,
-
-		// Symbol
-		// 4
-		getERSymbolX(269),
-		getERSymbolY(326),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-
-		// 5 
-		getERSymbolX(313),
-		getERSymbolY(326),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-		
-		// 6
-		getERSymbolX(295),
-		getERSymbolY(4),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-
-		// 7
-		getERSymbolX(289),
-		getERSymbolY(639),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-
-		// R 
-		// 8
-		getERSymbolX(301),
-		getERSymbolY(320),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-
-		// 9
-		getERSymbolX(300),
-		getERSymbolY(383),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-
-		// 10
-		getERSymbolX(450),
-		getERSymbolY(461),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,		
-		
-		// 11
-		getERSymbolX(432),
-		getERSymbolY(470),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,	
-
-		// 12
-		getERSymbolX(410),
-		getERSymbolY(427),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-
-		// 13
-		getERSymbolX(331),
-		getERSymbolY(511),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-		
-		// 14
-		getERSymbolX(352),
-		getERSymbolY(535),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-		
-		// 15
-		getERSymbolX(347),
-		getERSymbolY(495),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,			
-		
-		// 16
-		getERSymbolX(471),
-		getERSymbolY(674),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-
-		// E
-		// 17
-		getERSymbolX(286),
-		getERSymbolY(309),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-
-		// 18
-		getERSymbolX(150),
-		getERSymbolY(251),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-
-		// 19
-		getERSymbolX(284),
-		getERSymbolY(364),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-		
-		// 20
-		getERSymbolX(171),
-		getERSymbolY(237),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-
-		// 21
-		getERSymbolX(290),
-		getERSymbolY(185),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-		
-		// 22
-		getERSymbolX(290),
-		getERSymbolY(165),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-
-		// 23
-		getERSymbolX(175),
-		getERSymbolY(272),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-
-		// 24
-		getERSymbolX(256),
-		getERSymbolY(195),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-		
-		// 25
-		getERSymbolX(147),
-		getERSymbolY(97),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-
-		// 26
-		getERSymbolX(159),
-		getERSymbolY(92),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-
-		// 27
-		getERSymbolX(295),
-		getERSymbolY(96),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-
-		// 28
-		getERSymbolX(294),
-		getERSymbolY(51),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-
-		// 29
-		getERSymbolX(174),
-		getERSymbolY(115),
-		127.0f / 255.0f, 211.0f / 255.0f, 42.0f / 255.0f,
-};
+	
 
 	// ===Def data buffer===
 	GLuint myBufferID;
 	glGenBuffers(1, &myBufferID);
 	glBindBuffer(GL_ARRAY_BUFFER, myBufferID);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verts),
-		verts, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verts) + sizeof(indices) + sizeof(indices),
+		NULL, GL_STATIC_DRAW);
+	// ER Symbol Data
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(verts), verts);
 	// ===
 
 
@@ -251,35 +288,19 @@ void sendDataToOpenGL()
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (char*)(sizeof(float) * 2));
 
 	// ===Def idx buffer===
-	GLushort indices[] = { 
-		// BG
-		0,1,2, 
-		0,2,3,
-		// Symbol
-		4,5,6,
-		4,5,7,
-		// R
-		8,9,10,
-		8, 9, 11,
-		10,12,13,
-		13,10,14,
-		13, 16, 15,
-		// E
-		17, 18, 19,
-		17, 18, 20,
-		18,21,22,
-		18,21,23,
-		21,24,25,
-		21, 25, 26,
-		25,27,28,
-		25,27,29,
+	
 
-	};
-	GLuint indexBufferID;
-	glGenBuffers(1, &indexBufferID);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices),
-		indices, GL_STATIC_DRAW);
+	
+	//GLuint indexBufferID;
+	//glGenBuffers(1, &indexBufferID);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices),
+	//	indices, GL_STATIC_DRAW);
+	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, myBufferID);
+	glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, sizeof(verts), sizeof(indices), indices);
+	glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, sizeof(verts) + sizeof(indices), sizeof(indices), indices);
+
 	// ===
 }
 
@@ -344,6 +365,10 @@ void updateUniform() {
 	glUniform2f(offsetAttribLoc, triAtrans.position.x, triAtrans.position.y);
 }
 
+void updateUniform2() {
+	glUniform2f(offsetAttribLoc, triBtrans.position.x, triBtrans.position.y);
+}
+
 void initTriTrans2D() {
 	triAtrans.position = glm::vec2(-0.5f, -0.5f);
 	triBtrans.position = glm::vec2(0.5f, 0.5f);
@@ -370,7 +395,10 @@ void MeGlWindow::paintGL()
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glDrawElements(GL_TRIANGLES, 51, GL_UNSIGNED_SHORT, 0);
+	updateUniform();
+	glDrawElements(GL_TRIANGLES, 51, GL_UNSIGNED_SHORT, (void*)sizeof(verts));
+	updateUniform2();
+	glDrawElements(GL_TRIANGLES, 51, GL_UNSIGNED_SHORT, (void*)(sizeof(verts)+sizeof(indices)));
 }
 
 void MeGlWindow::keyPressEvent(QKeyEvent* e) 
@@ -404,7 +432,6 @@ void MeGlWindow::keyPressEvent(QKeyEvent* e)
 	default:
 		break;
 	}
-	updateUniform();
 	repaint();
 }
 
